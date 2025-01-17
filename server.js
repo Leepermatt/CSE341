77ab18ab6f2a75d6f70fcd4d255e81edd12e5ae3
Leepermatt <131700890+Leepermatt@users.noreply.github.com>
2025-01-17 15:52:31 -0800

    fixes for week 1

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const homeRoute = require('./routes/index');
+const homeRoute = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;

aff53e19a5899da6c7864082e0f63981a558d8d0
Leepermatt <131700890+Leepermatt@users.noreply.github.com>
2025-01-25 20:30:57 -0800

    week3.3

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const contactsController = require('../controllers/HomeController');
+const contactsController = require('../controllers/homeController');
 
 router.get('/', contactsController.getAll);
 
